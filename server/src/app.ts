0b078c2d2acb73fab06df3fc4f10e02e443e81b7
ptiza <ptizajar@gmail.com>
2026-09-21 14:35:31 +0300

    Serve client build from Express

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,16 @@
-// server/src/app.ts
 import express from 'express';
+import path from 'path';
 import stopListRoutes from './routes/stopList.routes';
 import { errorHandler } from './middleware/errorHandler';
 
 const app = express();
 
 app.use(express.json());
+
 app.use('/api', stopListRoutes);
+
+app.use(express.static(path.resolve(__dirname, '../../client/dist')));
+
 app.use(errorHandler);
 
 export default app;
